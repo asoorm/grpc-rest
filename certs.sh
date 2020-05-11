67cfe7e679620ae4e328e16da680e5268c71bea0
Ahmet Soormally <ahmet@mangomm.co.uk>
2020-05-11 07:32:02 +0100

    fix server.key location

--- certs.sh
+++ certs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-openssl genrsa -out server.key 2048
+openssl genrsa -out ./certs/server.key 2048
 openssl ecparam -genkey -name secp384r1 -out ./certs/server.key
 openssl req -nodes -subj '/CN=localhost' -new -x509 -sha256 -key ./certs/server.key -out ./certs/server.crt -days 365
